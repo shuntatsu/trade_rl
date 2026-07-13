d55620d7310e56cb34e52b356adeccc30ffc04f9
Shuntatsu <148511409+shuntatsu@users.noreply.github.com>
2026-07-14 00:46:08 +0900

    feat: export maintained dataset artifact APIs

--- trade_rl/data/__init__.py
+++ trade_rl/data/__init__.py
@@ -1,6 +1,14 @@
 """Market data contracts, sources, construction and validation."""
 
+from trade_rl.data.artifact import (
+    load_market_dataset_artifact,
+    write_market_dataset_artifact,
+)
 from trade_rl.data.builder import MarketDatasetBuilder
+from trade_rl.data.config import (
+    MarketDatasetBuildRequest,
+    load_market_build_request,
+)
 from trade_rl.data.contracts import (
     FeatureKind,
     FeatureSpec,
@@ -26,8 +34,12 @@ __all__ = [
     "MarketBuildConfig",
     "MarketDataSource",
     "MarketDataset",
+    "MarketDatasetBuildRequest",
     "MarketDatasetBuilder",
     "NormalizationMode",
     "RawMarketSeries",
     "VolumeUnit",
+    "load_market_build_request",
+    "load_market_dataset_artifact",
+    "write_market_dataset_artifact",
 ]

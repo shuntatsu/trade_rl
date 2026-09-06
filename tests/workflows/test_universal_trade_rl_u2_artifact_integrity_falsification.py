eb5e4206b4493d0de370a4fc3063f8d4d557c435
Shuntatsu <148511409+shuntatsu@users.noreply.github.com>
2026-09-06 23:51:12 +0900

    style: format U2 artifact integrity falsification

--- tests/workflows/test_universal_trade_rl_u2_artifact_integrity_falsification.py
+++ tests/workflows/test_universal_trade_rl_u2_artifact_integrity_falsification.py
@@ -14,7 +14,9 @@ from tests.integrations.test_universal_trade_rl_u2_selection_pairing import (
 )
 from tests.workflows.test_universal_trade_rl_u2_selection_final import _final_fixture
 from trade_rl.artifacts.hashing import content_digest
-from trade_rl.workflows.universal_trade_rl_u2_replay import UniversalTradeRLU2ReplayVariant
+from trade_rl.workflows.universal_trade_rl_u2_replay import (
+    UniversalTradeRLU2ReplayVariant,
+)
 
 pytest_plugins = ("tests.integrations.test_universal_trade_rl_u2_replay",)
 

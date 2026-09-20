c7be15caaa0a66c03feef294cea82c1eb7203759
Shuntatsu <shun020415@yahoo.co.jp>
2026-09-21 08:55:46 +0900

    feat: add sequential A2C fit adapter

--- trade_rl/strategies/rl/__init__.py
+++ trade_rl/strategies/rl/__init__.py
@@ -1,9 +1,21 @@
 """Reinforcement-learning maintained strategy family."""
 
+from trade_rl.strategies.rl.a2c import (
+    A2CFitMetadata,
+    A2CIntentStrategy,
+    fit_a2c_strategy,
+)
 from trade_rl.strategies.rl.ppo import (
     PPOIntentStrategy,
     PPOTradingEnv,
     fit_ppo_strategy,
 )
 
-__all__ = ["PPOIntentStrategy", "PPOTradingEnv", "fit_ppo_strategy"]
+__all__ = [
+    "A2CFitMetadata",
+    "A2CIntentStrategy",
+    "PPOIntentStrategy",
+    "PPOTradingEnv",
+    "fit_a2c_strategy",
+    "fit_ppo_strategy",
+]
